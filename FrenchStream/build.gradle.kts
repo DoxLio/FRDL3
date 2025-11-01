9cff299ed2e2073dd0774b03262290d116f4c225
DoxLio <120773936+DoxLio@users.noreply.github.com>
2025-11-01 04:53:17 +0100

    Update build.gradle.kts

--- FrenchStream/build.gradle.kts
+++ FrenchStream/build.gradle.kts
@@ -5,7 +5,7 @@ cloudstream {
 
     description = "French Stream est un site qui va récupèrer les films et séries sur des plateformes comme Disney+, Netflix, Amazon Prime Video, HBO, Apple TV , Wakanim, Viki... et vous les proposer Gratuitement!"
     language = "fr"
-    authors = listOf("ycngmn")
+    authors = listOf("DoxLio")
 
     status = 1 // 0: Down | 1: Ok
     tvTypes = listOf("Movie","TvSeries", "Anime")
